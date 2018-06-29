dce70043fc4562e4424539e5a22f7b2d51eb441f
jkkkkk <jkkkkk@vite.org>
2018-06-29 18:41:27 +0800

    Add token list api and token detail api

--- type/response/token.go
+++ type/response/token.go
@@ -1,5 +1,36 @@
 package response
 
+import (
+	"math/big"
+	"github.com/vitelabs/go-vite/ledger"
+	"github.com/gin-gonic/gin"
+)
+
+type TokenList struct {
+	tokenList []*Token
+}
+
+func NewTokenList (tokens []*ledger.Token) *TokenList{
+	var tokenList []*Token
+	for _, token := range tokens {
+		tokenList = append(tokenList, NewToken(token))
+	}
+	return &TokenList{
+		tokenList: tokenList,
+	}
+}
+
+
+func (t *TokenList) ToResponse () gin.H{
+	var tokenList []gin.H
+	for _, token := range t.tokenList {
+		tokenList = append(tokenList, token.ToResponse())
+	}
+	return gin.H{
+		"tokenList": tokenList,
+	}
+}
+
 type Token struct {
 	Name string
 	Id []byte
@@ -11,7 +42,29 @@ type Token struct {
 
 	Decimals int
 
-	TotalSupply string
+	TotalSupply *big.Int
 
-	TransactionNumber string
+	TransactionNumber *big.Int
+}
+
+func NewToken (ledgerToken *ledger.Token) *Token {
+	return &Token{
+
+	}
+}
+
+func (t*Token) ToResponse () gin.H {
+	return gin.H{
+		"name": t.Name,
+		"introduction": t.Introduction,
+
+
+		"symbol": t.Symbol,
+
+		"decimals": t.Decimals,
+
+		"totalSupply": t.TotalSupply.String(),
+
+		"transactionNumber": t.TransactionNumber.String(),
+	}
 }
